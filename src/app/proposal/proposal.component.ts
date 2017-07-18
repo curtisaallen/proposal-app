00b6fe36714d421426aa2b67a6dae10891d4e70d
Curtis Allen <callen@callen-m01.vmware.com>
2017-07-17 22:35:41 -0400

    add proposal class

--- src/app/proposal/proposal.component.ts
+++ src/app/proposal/proposal.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Proposal }  from './proposal';
 @Component({
   selector: 'app-proposal',
   templateUrl: './proposal.component.html',
   styleUrls: ['./proposal.component.css']
 })
 export class ProposalComponent implements OnInit {
-
+  proposalOne: Proposal = new Proposal(15, 'Wpexchange', 'http://wpexchange.com', 'Wordpress site', 150, 120,15, 'curtisaallen@gmail.com');
+  proposalTwo: Proposal = new Proposal(15, 'google', 'http://wpexchange.com', 'Wordpress site', 150, 120,15, 'curtisaallen@gmail.com');
+  proposalThree: Proposal = new Proposal(15, 'Divine 9 Apparel', 'http://wpexchange.com', 'Wordpress site', 150, 120,15, 'curtisaallen@gmail.com');
+  
+  proposals: Proposal[] = [
+    this.proposalOne, 
+    this.proposalTwo, 
+    this.proposalThree
+  ];
   constructor() { }
 
   ngOnInit() {
